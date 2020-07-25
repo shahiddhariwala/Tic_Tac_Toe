387a4d12e27fb2e81a71762e3aade7261e5bfb2b
shahiddhariwala <shahid.dhariwala@zotalabs.com>
2020-07-25 20:22:45 +0530

    Basic Setup done

---
+++ tic_tac_toe_react/src/container/Board.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component} from 'react';
+
+class Board extends Component
+{
+    render()
+    {
+        return (
+            <>
+            I Love React 
+            </>
+        );
+    }
+}
+
+export default Board;
