6e2ccdc6181ae39db584c3bb023666c3d964378a
shahiddhariwala <shahid.dhariwala@zotalabs.com>
2020-07-25 21:12:35 +0530

    Assets and Basic Layout Implemented

--- tic_tac_toe_react/src/container/Board.js
+++ tic_tac_toe_react/src/container/Board.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Component} from 'react';
-
+import Grid from "../components/Grid";
 class Board extends Component
 {
     render()
     {
         return (
             <>
-            I Love React 
+            I Love React - Shahid Dhariwala
+            <Grid/>
             </>
         );
     }
